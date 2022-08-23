89258d4a89b19bfb475c0cc353658f96ab3f32fa
TarCode <tarcode33@gmail.com>
2022-08-23 22:14:06 +0200

    Added user model and controller

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
+end
